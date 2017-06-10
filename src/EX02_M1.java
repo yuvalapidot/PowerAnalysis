5bd35668b99c3ece114f3e2deca1d4e0ddec13a5
yuval <yuvalapidot@gmail.com>
2017-06-10 22:02:56 +0300

    Added basic send request function

--- src/EX02_M1.java
+++ src/EX02_M1.java
@@ -1,3 +1,9 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class EX02_M1 {
 
@@ -5,11 +11,28 @@ public class EX02_M1 {
 
     }
 
-    public void download_power_traces(String filename, String serverUrl, int number_of_power_Traces) {
+    public void download_power_traces(String filename, String serverUrl, int number_of_power_Traces) throws IOException {
+        String[] traces = new String[number_of_power_Traces];
+        for (int i = 0; i < number_of_power_Traces; i++) {
+            traces[i] = sendTraceRequest(serverUrl);
+        }
 
     }
 
+    private String sendTraceRequest(String serverUrl) throws IOException {
+        URL urlObject = new URL(serverUrl);
+        // Establish the connection
+        HttpURLConnection connection = (HttpURLConnection) urlObject.openConnection();
+        // Set request method to GET
+        connection.setRequestMethod("GET");
+        // Create a buffered reader to read from the response
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        String trace = bufferedReader.readLine();
+        bufferedReader.close();
+        return trace;
+    }
+
     public void getMeansVariances(String filename) {
-        
+
     }
 }
