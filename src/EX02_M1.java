e87632ba389dff19908a645c19c817590aaeadfd
yuval <yuvalapidot@gmail.com>
2017-06-10 20:56:24 +0300

    Initial commit

---
+++ src/EX02_M1.java
@@ -0,0 +1,15 @@
+
+public class EX02_M1 {
+
+    public void Main(String[] args) {
+
+    }
+
+    public void download_power_traces(String filename, String serverUrl, int number_of_power_Traces) {
+
+    }
+
+    public void getMeansVariances(String filename) {
+        
+    }
+}
